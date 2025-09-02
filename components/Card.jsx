5fa5c1d8c8809a85cc57ffd95e88a5352c0f7032
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-09-02 23:30:50 +0100

    card structure and styling created for now. may get rid of fixed width/height

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,5 +1,17 @@
+import Placeholder from "../images/helpimg.jpg"
+
 export default function Card() {
     return (
-        <h1>test lol</h1>
+        <div className="card">
+            <img src={Placeholder} />
+            <div className="card-right-side">
+            <h2>Signal for Help</h2>
+            <p>
+                If you’re lost without cell service, three short signals (whistle blasts, shouts, or flashes) 
+                is the universal distress call.  
+                Pause, then repeat until spotted.
+            </p>
+            </div>
+        </div>
     )
 }
