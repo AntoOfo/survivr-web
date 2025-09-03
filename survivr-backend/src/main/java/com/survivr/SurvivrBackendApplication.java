29c6fcdb2cf2509b65d3e3b985a99a84255a3b43
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-09-03 20:21:25 +0100

    backend springboot folder added

---
+++ survivr-backend/src/main/java/com/survivr/SurvivrBackendApplication.java
@@ -0,0 +1,13 @@
+package com.survivr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurvivrBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurvivrBackendApplication.class, args);
+	}
+
+}
---
+++ survivr-backend/src/test/java/com/survivr/SurvivrBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.survivr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurvivrBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
