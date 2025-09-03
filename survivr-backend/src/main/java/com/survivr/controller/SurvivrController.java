09835567f56e30cc09eb8ddecae9e0d71a4044d2
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-09-03 21:31:23 +0100

    api and webclient variable set up

--- survivr-backend/src/main/java/com/survivr/controller/SurvivrController.java
+++ survivr-backend/src/main/java/com/survivr/controller/SurvivrController.java
@@ -3,11 +3,22 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.survivr.controller;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import reactor.core.publisher.Mono;
 
 /**
  *
  * @author anton
  */
+@RestController
 public class SurvivrController {
+    
+    @Value("${groq.api.key")
+    private String groqApiKey;
+    
+    private final WebClient webClient = WebClient.create();
     
 }
