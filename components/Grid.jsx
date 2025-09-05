caf1e17c58ea66bd6d3ec1e8dc4854ddfb9dfde2
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-09-05 18:57:14 +0100

    building useffect

--- components/Grid.jsx
+++ components/Grid.jsx
@@ -1,9 +1,15 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Card from "./Card"
 
 export default function Grid() {
-    
+
     const [hacks, setHacks] = useState([]);
+
+    useEffect(() =>
+        // call api
+        fetch("http://localhost:8080/lifehacks")
+            .then((res) => res.json())  // convert 2 json
+            .then)
     return (
         <main>
             <h2 className="grid-title">Today's Picks</h2>
