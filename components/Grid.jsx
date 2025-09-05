f1894909e55efb264fad005a2c1c7f310dea0e34
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-09-05 18:46:32 +0100

    hacks list state initiliased

--- components/Grid.jsx
+++ components/Grid.jsx
@@ -1,18 +1,13 @@
+import { useState } from "react"
 import Card from "./Card"
 
 export default function Grid() {
+    
+    const [hacks, setHacks] = useState([]);
     return (
         <main>
             <h2 className="grid-title">Today's Picks</h2>
             <div className="grid">
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
                 <Card />
             </div>
         </main>
