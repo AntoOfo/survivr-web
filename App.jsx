0598247012dbf5d5bcbec727d58a082a908fbb4c
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-09-06 18:16:59 +0100

    search functionality working

--- App.jsx
+++ App.jsx
@@ -1,11 +1,18 @@
 import Header from "./components/Header"
 import Grid from "./components/Grid"
+import { useState } from "react"
 
 export default function App() {
+  // hold current search
+  const [searchQuery, setSearchQuery] = useState("");
+
   return (
     <>
-    <Header />
-    <Grid />
+    <Header 
+      searchQuery={searchQuery}
+      setSearchQuery={setSearchQuery} />
+    <Grid 
+      searchQuery={searchQuery}/>
     </>
   )
 }
