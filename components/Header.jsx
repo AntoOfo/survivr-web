9a64edc506077e8a7a81580df3ee3a2945518b75
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-09-06 17:10:56 +0100

    search func created in header file

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,15 @@
 import FilterIcon from "../images/filter.png"
 
-export default function Header() {
+export default function Header({searchQuery, setSearchQuery}) {
     return (
         <header>
             <h1>SURVIVR</h1>
-            <input type="text" placeholder="Search.."></input>
+            <input 
+                type="text" 
+                placeholder="Search.."
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}  // updates searchQuery
+                />
             <button>
                 <img src={FilterIcon}/>
             </button>
